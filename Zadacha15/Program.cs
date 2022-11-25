fdb64511cee5b8f92bc88d10eae7cc4ae17c886e
IrinchinovZh <exp.bur@gmail.com>
2022-11-25 17:30:54 +0800

    add zadacha15

---
+++ Zadacha15/Program.cs
@@ -0,0 +1,21 @@
+﻿// Console.Clear();
+// Console.Write("Введите день недели: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// while (n < 1 || n > 7)
+// {
+//     Console.Write("Вы ошиблись!\nВведите день недели: ");
+//     n = Convert.ToInt32(Console.ReadLine());
+// }
+
+Console.Clear();
+Console.Write("Vvedite cifru dna nedeli: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n<1||n>7)
+{
+    Console.Write("Oshibka \nVvedite zanovo: ");
+    n= Convert.ToInt32(Console.ReadLine());
+}
+if (n==6||n==7)
+Console.WriteLine("Den' vihodnoi");
+else
+Console.WriteLine("Budnii dni");
