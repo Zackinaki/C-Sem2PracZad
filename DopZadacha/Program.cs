f3afcabec5deafbc29399d5db5542f79bc14aec0
IrinchinovZh <exp.bur@gmail.com>
2022-11-25 21:44:43 +0800

    Add Dopzadacha

---
+++ DopZadacha/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Vvdedite chisla: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int maxmin=0;
+do{
+    int b = Convert.ToInt32(Console.ReadLine());
+    if (b==0)
+       break;
+    if(b>a){
+        maxmin=a;
+        a=b;
+    }
+    else if(b>maxmin)
+    maxmin=b;
+}
+while(true);
+Console.WriteLine($"Vtoroe no velichine chislo: {maxmin}");
+
