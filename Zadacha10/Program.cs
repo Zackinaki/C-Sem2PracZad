e1f85accfd616f4633c57bc0fd474cc345cf0c00
IrinchinovZh <exp.bur@gmail.com>
2022-11-25 13:47:13 +0800

    change zadacha10

--- Zadacha10/Program.cs
+++ Zadacha10/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.Clear();
+Console.Write("Vvdedite trexznachnoe chislo: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int a1=(a/10)%10;
+Console.WriteLine($"Vtoraa cifra chisla: {a1}");
